d93ee3dec04fcf6c1ba8932098137692026974b5
Justin Wiblin <the_lone_devil@justin-wiblin.tk>
2013-05-21 21:25:37 +0100

    cleaned imports a bit

--- src/main/java/com/github/thelonedevil/AgeChecker/App.java
+++ src/main/java/com/github/thelonedevil/AgeChecker/App.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Map;
 
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
